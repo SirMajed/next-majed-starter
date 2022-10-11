1099ca42a46f91fdddebe29182db8d320106cfdf
Majed Alhasin <sidrozzg@gmail.com>
2022-10-11 12:49:33 +0300

    Added more folders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="p-2">Edit this from /pages/index.tsx</h1>
+      <h1 className="p-2">Edit this from /pages/index.tsx file</h1>
     </>
   )
 }
